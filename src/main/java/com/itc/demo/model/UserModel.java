6f8e12df3891187135c702663409da0808388b38
M.Pattnaik <m.pattnaik@itcinfotech.com>
2024-08-08 10:21:23 +0000

    modified UserModel

--- src/main/java/com/itc/demo/model/UserModel.java
+++ src/main/java/com/itc/demo/model/UserModel.java
@@ -11,16 +11,27 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+
 public class UserModel {
 
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String userID = null;
     private String firstName;
     private String lastName;
-    @Id
+
     @Column(unique = true,nullable = false)
     private String email;
     private String password;
     private String profilePicture = "https://thumbs.dreamstime.com/z/beautiful-display-pink-white-red-petunias-summer-day-coast-pole-50746151.jpg";
     private String phoneNumber;
+
+    public UserModel(String firstName, String lastName, String email, String password, String profilePicture, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.profilePicture = profilePicture;
+        this.phoneNumber = phoneNumber;
+    }
 }
